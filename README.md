2d3715f195ddbb07c806fbc8de04858426a918ac
Eugene Naumenko <eugene.naumenko@gmail.com>
2019-02-16 11:33:29 +0200

    dist

--- README.md
+++ README.md
@@ -13,3 +13,37 @@
 
   This command makes use of `sws` - a simple Haskell web server - to serve static content from the `dist` directory. If you have no `sws` installed feel free to use any other preferred web server serving files from the same directory.
 
+## Use for yourself 
+
+- Copy the contents of the `dist` directory to your web server path.
+- Create a Github Gist containing a json representation of the `Data.Tree` site tree (or use the example below.). 
+- Replace a root gist id in the `index.html` file with the id of your root gist:
+
+  `<script>window.RootG=JSON.stringify({"rootGist":"-your-gist-id-here-"})</script>`
+- Open the page in a browser.
+
+
+## Example root gist
+
+```json
+
+[
+    [
+        {
+            "dataSource": "<a gist id here>",
+            "title": "Menu 1",
+            "path": "menu1"
+        },
+        []
+    ],
+    [
+        {
+            "dataSource": "<another gist id here>",
+            "title": "Menu 2",
+            "path": "menu2"
+        },
+        []
+    ]
+]P
+
+```
---
+++ dist/rte/build-config.js
@@ -0,0 +1,82 @@
+﻿/**
+ * @license Copyright (c) 2003-2019, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/license
+ */
+
+/**
+ * This file was added automatically by CKEditor builder.
+ * You may re-use it at any time to build CKEditor again.
+ *
+ * If you would like to build CKEditor online again
+ * (for example to upgrade), visit one the following links:
+ *
+ * (1) https://ckeditor.com/cke4/builder
+ *     Visit online builder to build CKEditor from scratch.
+ *
+ * (2) https://ckeditor.com/cke4/builder/42fc9bfdb2225d93001f8f827317cb94
+ *     Visit online builder to build CKEditor, starting with the same setup as before.
+ *
+ * (3) https://ckeditor.com/cke4/builder/download/42fc9bfdb2225d93001f8f827317cb94
+ *     Straight download link to the latest version of CKEditor (Optimized) with the same setup as before.
+ *
+ * NOTE:
+ *    This file is not used by CKEditor, you may remove it.
+ *    Changing this file will not change your CKEditor configuration.
+ */
+
+var CKBUILDER_CONFIG = {
+	skin: 'minimalist',
+	preset: 'basic',
+	ignore: [
+		'.DS_Store',
+		'.bender',
+		'.editorconfig',
+		'.gitattributes',
+		'.gitignore',
+		'.idea',
+		'.jscsrc',
+		'.jshintignore',
+		'.jshintrc',
+		'.mailmap',
+		'.npm',
+		'.travis.yml',
+		'bender-err.log',
+		'bender-out.log',
+		'bender.ci.js',
+		'bender.js',
+		'dev',
+		'gruntfile.js',
+		'less',
+		'node_modules',
+		'package.json',
+		'tests'
+	],
+	plugins : {
+		'autolink' : 1,
+		'basicstyles' : 1,
+		'blockquote' : 1,
+		'clipboard' : 1,
+		'colorbutton' : 1,
+		'colordialog' : 1,
+		'divarea' : 1,
+		'enterkey' : 1,
+		'entities' : 1,
+		'floatingspace' : 1,
+		'font' : 1,
+		'format' : 1,
+		'horizontalrule' : 1,
+		'image' : 1,
+		'indentlist' : 1,
+		'link' : 1,
+		'list' : 1,
+		'quicktable' : 1,
+		'removeformat' : 1,
+		'sourcearea' : 1,
+		'stylescombo' : 1,
+		'toolbar' : 1,
+		'undo' : 1
+	},
+	languages : {
+		'en' : 1
+	}
+};
